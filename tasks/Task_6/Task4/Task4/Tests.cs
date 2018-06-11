fd3db4d69c5bf0385feb7e25852d35a5264117ef
Baris Senkal <ic17b038@technikum-wien.at>
2018-06-12 00:49:17 +0200

    add Task6

---
+++ tasks/Task_6/Task4/Task4/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Catch(() =>
+            { var x = new Mitarbeiter(0, "Peter"); });
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var y = new Mitarbeiter(1000, "Hugo");
+            Assert.IsTrue(y.Gehaltt == 1000);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Catch(() =>
+            { var b = new Kunden("name", 16); });
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.Catch(() =>
+            { var y = new Mitarbeiter(1000, ""); });
+        }
+
+        [Test]
+        public void Test6()
+        {
+            var t = new Mitarbeiter(1500,"Klaus");
+            Assert.IsTrue(t.Gehaltt == 1500);
+            Assert.IsTrue(t.Namee == "Klaus");
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Assert.Catch(() => { var h = new Kunden("", 0); });
+            Assert.Catch(() => { var h = new Kunden("h", 0); });
+            Assert.Catch(() => { var h = new Kunden("", 14); });
+
+        }
+
+        [Test]
+        public void Test8()
+        {
+            Assert.Catch(() =>
+            { var b = new Kunden("Martha", 0); });
+        }
+
+    }
+}
